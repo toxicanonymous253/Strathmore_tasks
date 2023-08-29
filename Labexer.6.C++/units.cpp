cfa70b71a8ae2f226ab1ac53f9bcbb70f9c8b749
toxicfrank <ngugifrancis930@gmai.com>
2023-08-29 13:48:01 +0300

    tasks

--- Labexer.6.C++/units.cpp
+++ Labexer.6.C++/units.cpp
@@ -14,12 +14,24 @@ int main(void)
     }
     else if(units_consumed <= 150)
     {
-        toatal_bill = 50 * 10 + (units_consumed - 50) * 20; 
+        toatal_bill = 50 * 10 + ((units_consumed - 50) * 20); 
     }
     else if(units_consumed <= 250)
     {
-        toatal_bill = 50 * 10 
+        toatal_bill = 50 * 10 + 100 * 20 + ((units_consumed - 150) * 30);
     }
+    else if(units_consumed > 250)
+    {
+        toatal_bill = 50 * 10 + 100 * 20 + 100 * 30 + ((units_consumed - 250) * 50);
+    }
+    else
+    {
+        cout << "Invalid input";
+    }
+    int surcharge = (0.2 * toatal_bill) + toatal_bill;
+    int final_bill = surcharge;
+
+    cout<<final_bill<<endl;
 
     return (0);
 }
