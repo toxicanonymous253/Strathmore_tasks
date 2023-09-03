92895e02c12300c1d6ae37ca8c0a74bd4b16b2fc
toxicfrank <ngugifrancis930@gmai.com>
2023-09-03 16:32:10 +0300

    task

--- Labexer.7/purchase.py
+++ Labexer.7/purchase.py
@@ -1,1 +1,19 @@
-#prom
+#prommpt the user fo the cost of two items
+item1_cost = float(input("Enter the cost of the first item: ksh"))
+item2_cost = float(input("Enter the cost of the second item: ksh"))
+
+#claculate the total cost
+total_cost = item1_cost + item2_cost
+
+#prommpt the user for their payment
+payment = float(input("Enter your payment amount: ksh"))
+
+#check if the payment is less, enough or more
+if payment < total_cost:
+    amount_due = total_cost - payment
+    print(f"You still owe ksh{amount_due:.2f}")
+elif payment == total_cost:
+    print("Thank you for your payment.")
+else:
+    chenge = payment - total_cost
+    print(f"Thank you for your payment, here is your change ksh{chenge:.2f}")
