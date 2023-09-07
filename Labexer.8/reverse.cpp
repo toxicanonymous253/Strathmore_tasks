ce42a3df7ff10a8f805888533af59e9ac44687f2
toxicfrank <ngugifrancis930@gmai.com>
2023-09-07 20:59:55 +0300

    task

---
+++ Labexer.8/reverse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//will print numbers using for loop
+int main(void)
+{
+    int i;
+    
+    for(i = 7; i > -5; i--)
+    {
+        cout<<" "<<i ;
+    }
+    cout<<"\n";
+    return (0);
+}
