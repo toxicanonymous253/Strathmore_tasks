9b03f3f4041be462265094a773592fca25a786c5
toxicfrank <ngugifrancis930@gmai.com>
2023-09-13 07:59:05 +0300

    task

--- Labexer.8/savings.cpp
+++ Labexer.8/savings.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 int main(void)
 {
+    int savings = 2;
+    int iterator = 6;
+    int total = 0;
+
+    while (iterator > 0)
+    {
+        total += savings;
+        savings *= 2;
+        iterator--;
+    }
+    cout<<" "<< savings<<endl;
+
     return (0);
 }
