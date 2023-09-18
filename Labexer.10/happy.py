3f7c8ca5709bc462695b83373e9e9260cab9676b
toxicfrank <ngugifrancis930@gmai.com>
2023-09-18 09:06:46 +0300

    tasks

---
+++ Labexer.10/happy.py
@@ -0,0 +1,10 @@
+# Get the user's age
+age = int(input("Enter your age: "))
+
+# Initialize a counter
+year = 1
+
+# Use a while loop to print "Happy Birthday!" for each year of age
+while year <= age:
+    print("Happy Birthday!")
+    year += 1
