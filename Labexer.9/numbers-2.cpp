f343cdc345691e05557f61c1f564042d1507152f
toxicfrank <ngugifrancis930@gmai.com>
2023-09-18 06:03:58 +0300

    task

---
+++ Labexer.9/numbers-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Use a for loop to iterate from 12 to 28 with a step of 2
+    for (int i = 12; i <= 28; i += 2) {
+        cout << i << " ";
+    }
+
+    // Add a newline at the end
+    cout << std::endl;
+
+    return 0;
+}
