93d9312caf5011aaaef7dd5246b6778c932f6825
toxicfrank <ngugifrancis930@gmai.com>
2023-09-18 06:06:04 +0300

    task

---
+++ Labexer.9/sorry.py
@@ -0,0 +1,6 @@
+# Get the number of times from the user
+num_times = int(input("Enter the number of times you want to say 'I am sorry': "))
+
+# Use a for loop to print the statement the specified number of times
+for i in range(num_times):
+    print("I am sorry")
