9d05c995c513a240d5da14e34c401fd80c389120
toxicfrank <ngugifrancis930@gmai.com>
2023-09-18 05:59:02 +0300

    task

---
+++ Labexer.9/numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int i = 12;
+
+    do{
+        i++;
+        i++;
+        cout<<" "<<i;
+    }while(i < 28);
+    cout<<"\n";
+    return (0);
+}
