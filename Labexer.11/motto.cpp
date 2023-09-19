4b8be82ad5c6ca0f30a68b21099823508143f529
toxicfrank <ngugifrancis930@gmai.com>
2023-09-19 16:08:27 +0300

    task

---
+++ Labexer.11/motto.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// function prototype
+void motto();
+
+int main(void)
+{
+    motto();
+
+    return (0);
+}
+
+void motto()
+{
+    cout<<"\t\t\tStrathmore university motto is\n\t\t\t\tThat all may be one"<<endl;
+    return;
+}
+
