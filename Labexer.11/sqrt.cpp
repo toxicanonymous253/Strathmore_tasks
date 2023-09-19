366e14c5638baebacceedb733a299eb3d62236cf
toxicfrank <ngugifrancis930@gmai.com>
2023-09-19 16:03:54 +0300

    tasks

---
+++ Labexer.11/sqrt.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(void)
+{
+    float sqroot = sqrt(42);
+
+    cout<<fixed<<setprecision(3)<<sqroot<<endl;
+
+    return (0);
+}
