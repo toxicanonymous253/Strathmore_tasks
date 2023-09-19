91fcd406991c245d35e9ce30e4095926de667295
toxicfrank <ngugifrancis930@gmai.com>
2023-09-19 15:58:21 +0300

    task

---
+++ Labexer.11/power.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(void)
+{
+    int power = pow(2, 18);
+
+    int cal = power - 1;
+    cout<<cal<<endl;
+
+    return (0);
+}
